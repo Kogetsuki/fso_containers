b7d93c160c589b6c855767783dbf6f730090a865
Kogetsuki <rozenbastien@gmail.com>
2025-12-07 13:23:54 +0100

    first commit

---
+++ todo-app/todo-backend/routes/todos.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const { Todo } = require('../mongo')
+
+const router = express.Router()
+const singleRouter = express.Router
+
+
+// GET todos listening
+router.get('/', async (_, res) => {
+  const todos = await Todo.find({})
+  res.send(todos)
+})
+
+// POST todo to listing
+router.post('/', async (req, res) => {
+  const todo = await Todo.create({
+    text: req.body.text,
+    done: false
+  })
+
+  res.send(todo)
+})
+
+
+const findByIdMiddleware = async (req, res, next) => {
+  const { id } = req.params
+  req.todo = await Todo.findById(id)
+
+  if (!req.todo)
+    return res.sendStatus(404)
+
+  next()
+}
+
+// DELETE todo
+singleRouter.delete('/', async (req, res) => {
+  await req.todo.delete()
+  res.sendStatus(200)
+})
+
+// GET todo
+singleRouter.get('/', async (req, res) => {
+  res.sendStatus(405)
+})
+
+// PUT todo
+singleRouter.put('/', async (req, res) => {
+  res.sendStatus(405)
+})
+
+router.use('/:id', findByIdMiddleware, singleRouter)
+
+
+module.exports = router
---
+++ todo-app/todo-frontend/src/Todos/Form.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const TodoForm = ({ createTodo }) => {
+  const [text, setText] = useState('')
+
+  const onChange = ({ target }) =>
+    setText(target.value)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createTodo({ text })
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' name='text' value={text} onChange={onchange} />
+      <button type='submit'>Submit</button>
+    </form>
+  )
+}
+
+
+export default TodoForm
